745df89f6a9a44bc7c3abda3b836659e726db198
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-02-17 16:01:59 +0200

    update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,8 +1,31 @@
-import axios from 'axios';
+export function imagesTemplate(arrPict, tagGallery) {
+    const gallery = document.querySelector(tagGallery);
+    gallery.innerHTML = '';
+  
+    const markup = arrPict.map((image) =>
+      `<li class="gallery-item">
+        <a class='gallery-link' href="${image.largeImageURL}">
+          <img class="li-img" src="${image.webformatURL}" alt="${image.tags}" />
+          <div class="li-text">
+            <table class="table">
+              <tr><td>Likes</td><td>Views</td><td>Comment</td><td>Downloads</td></tr>
+              <tr><td>${image.likes}</td><td>${image.views}</td><td>${image.comments}</td><td>${image.downloads}</td></tr>
+            </table>
+          </div>
+        </a>
+      </li>`
+    ).join("");
+  
+    gallery.insertAdjacentHTML("afterbegin", markup);
+  
+    const lightbox = new SimpleLightbox(tagGallery + " a", {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.refresh();
+  }
+  
 
-import iziToast from "izitoast";
-"izitoast/dist/css/iziToast.min.css";
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
